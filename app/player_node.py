3939862ffe15412ae127cb7b8905fec85791c6b1
Asmita Bhandare <20106482@tafe.wa.edu.au>
2024-02-13 21:04:17 +0800

    Added class PlayerNode

--- app/player_node.py
+++ app/player_node.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 # from typing import TYPE_CHECKING
 from player import Player
 
-
 class PlayerNode:
     def __init__(self, player: Player):
         self._player = player
