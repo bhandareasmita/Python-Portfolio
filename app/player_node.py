03f868f37a7ba917b9875a6ce1a45738f87255e4
Asmita Bhandare <20106482@tafe.wa.edu.au>
2024-02-16 22:25:41 +0800

    Added properties to the PlayaerNode class and implemented __str__ method

--- app/player_node.py
+++ app/player_node.py
@@ -2,6 +2,21 @@ from __future__ import annotations
 # from typing import TYPE_CHECKING
 from player import Player
 
+
 class PlayerNode:
     def __init__(self, player: Player):
         self._player = player
+        self.next = None
+        self.prev = None
+
+    @property
+    def player(self) -> Player:
+        return self._player
+
+    @property
+    def key(self):
+        return self._player.uid
+
+    def __str__(self):
+        return f"{self.player.uid} : {self.player.name}"
+
