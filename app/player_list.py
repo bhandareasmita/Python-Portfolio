a1318b70daacf67a79d2d39c843be1a8c292e5f4
Asmita Bhandare <20106482@tafe.wa.edu.au>
2024-02-16 23:52:41 +0800

    Added private instance variable to the PlayerList class and added test cases

--- app/player_list.py
+++ app/player_list.py
@@ -5,9 +5,13 @@ from player import Player
 class PlayerList:
     def __init__(self):
         self._head = None
+        self._tail = None
 
     def get_head(self):
         return self._head
+
+    def get_tail(self):
+        return self._tail
 
     def is_empty(self):
         if self._head is None:
@@ -18,10 +22,10 @@ class PlayerList:
         new_player = PlayerNode(Player(player_id, player_name))
         if self.is_empty():
             self._head = new_player
+            self._tail = self._head
         else:
             new_player.next = self._head
             new_player.prev = None
-            self._head.prev = new_player
             self._head = new_player
 
 
--- test/player_test.py
+++ test/player_test.py
@@ -16,6 +16,7 @@ class PlayerTest(unittest.TestCase):
         dl_list = PlayerList()
         dl_list.push('34', 'Tom')
         self.assertEqual(dl_list._head.key, '34')
+        self.assertEqual(dl_list._tail.key, '34')
         self.assertIsNone(dl_list._head.next)
 
     def test_for_adding_new_player_when_list_contains(self):
@@ -25,6 +26,19 @@ class PlayerTest(unittest.TestCase):
         self.assertIsNotNone(dl_list._head)
         self.assertIsNotNone(dl_list.is_empty())
         self.assertEqual(dl_list._head.key, '56')
+        self.assertEqual(dl_list._tail.key, '14')
+
+    def test_for_head_and_tail_when_list_empty(self):
+        dl_list = PlayerList()
+        self.assertIsNone(dl_list._head)
+        self.assertIsNone(dl_list._tail)
+
+    def test_for_head_and_tail_when_list_contains_one_element(self):
+        dl_list = PlayerList()
+        dl_list.push('23', 'Josh')
+        self.assertEqual(dl_list._head.player.name, 'Josh')
+        self.assertEqual(dl_list._tail.player.name, 'Josh')
+        self.assertIsNotNone(dl_list.is_empty())
 
 
 if __name__ == '__main__':
