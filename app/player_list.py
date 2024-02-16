439f51f80d8192ae927f855b8911db60f56fbebb
Asmita Bhandare <20106482@tafe.wa.edu.au>
2024-02-17 00:35:16 +0800

    Implement method insert_at_the_tail to PlayerList class and added test cases

--- app/player_list.py
+++ app/player_list.py
@@ -10,22 +10,42 @@ class PlayerList:
     def get_head(self):
         return self._head
 
+    def set_head(self, node):
+        self._head = node
+
     def get_tail(self):
         return self._tail
 
+    def set_tail(self, node):
+        self._tail = node
+
     def is_empty(self):
-        if self._head is None:
+        if self.get_head() is None:
             return True
         return False
 
     def push(self, player_id, player_name):
         new_player = PlayerNode(Player(player_id, player_name))
         if self.is_empty():
-            self._head = new_player
-            self._tail = self._head
+            self.set_head(new_player)
+            self.set_tail(new_player)
         else:
-            new_player.next = self._head
+            new_player.next = self.get_head()
+            self.get_head().prev = new_player
+            self.set_head(new_player)
             new_player.prev = None
-            self._head = new_player
+
+    def insert_at_the_tail(self, player_id, player_name):
+        new_player = PlayerNode(Player(player_id, player_name))
+        if self.is_empty():
+            self.set_head(new_player)
+            self.set_tail(new_player)
+        else:
+            last_node = self.get_tail()
+            new_player.prev = last_node
+            last_node.next = new_player
+            new_player.next = None
+            self.set_tail(new_player)
+
 
 
--- test/player_test.py
+++ test/player_test.py
@@ -15,9 +15,9 @@ class PlayerTest(unittest.TestCase):
     def test_for_adding_new_player_when_list_empty(self):
         dl_list = PlayerList()
         dl_list.push('34', 'Tom')
-        self.assertEqual(dl_list._head.key, '34')
-        self.assertEqual(dl_list._tail.key, '34')
-        self.assertIsNone(dl_list._head.next)
+        self.assertEqual(dl_list.get_head().key, '34')
+        self.assertEqual(dl_list.get_tail().key, '34')
+        self.assertIsNone(dl_list.get_head().next)
 
     def test_for_adding_new_player_when_list_contains(self):
         dl_list = PlayerList()
@@ -25,20 +25,34 @@ class PlayerTest(unittest.TestCase):
         dl_list.push('56', 'Ann')
         self.assertIsNotNone(dl_list._head)
         self.assertIsNotNone(dl_list.is_empty())
-        self.assertEqual(dl_list._head.key, '56')
-        self.assertEqual(dl_list._tail.key, '14')
+        self.assertEqual(dl_list.get_head().key, '56')
+        self.assertEqual(dl_list.get_tail().key, '14')
 
     def test_for_head_and_tail_when_list_empty(self):
         dl_list = PlayerList()
-        self.assertIsNone(dl_list._head)
-        self.assertIsNone(dl_list._tail)
+        self.assertIsNone(dl_list.get_head())
+        self.assertIsNone(dl_list.get_tail())
 
     def test_for_head_and_tail_when_list_contains_one_element(self):
         dl_list = PlayerList()
         dl_list.push('23', 'Josh')
-        self.assertEqual(dl_list._head.player.name, 'Josh')
-        self.assertEqual(dl_list._tail.player.name, 'Josh')
+        self.assertEqual(dl_list.get_head().player.name, 'Josh')
+        self.assertEqual(dl_list.get_tail().player.name, 'Josh')
         self.assertIsNotNone(dl_list.is_empty())
+
+    def test_for_insert_at_the_tail_when_list_empty(self):
+        dl_list = PlayerList()
+        dl_list.insert_at_the_tail('3', 'Sammy')
+        self.assertEqual(dl_list.get_head().player.name, 'Sammy')
+        self.assertEqual(dl_list.get_tail().player.uid, '3')
+
+    def test_for_insert_at_the_tail_when_list_contains(self):
+        dl_list = PlayerList()
+        dl_list.push('1', 'Asmita')
+        dl_list.push('2', 'Rose')
+        dl_list.insert_at_the_tail('3', 'Sammy')
+        self.assertEqual(dl_list.get_tail().player.name, 'Sammy')
+        self.assertEqual(dl_list.get_tail().prev.player.name, 'Asmita')
 
 
 if __name__ == '__main__':
