2eb27220bbaa0cf51041222a05197e9b4407b992
Asmita Bhandare <20106482@tafe.wa.edu.au>
2024-02-16 23:15:35 +0800

    Implement push method and added test cases

--- app/player_list.py
+++ app/player_list.py
@@ -1,3 +1,27 @@
+from player_node import PlayerNode
+from player import Player
+
+
 class PlayerList:
     def __init__(self):
         self._head = None
+
+    def get_head(self):
+        return self._head
+
+    def is_empty(self):
+        if self._head is None:
+            return True
+        return False
+
+    def push(self, player_id, player_name):
+        new_player = PlayerNode(Player(player_id, player_name))
+        if self.is_empty():
+            self._head = new_player
+        else:
+            new_player.next = self._head
+            new_player.prev = None
+            self._head.prev = new_player
+            self._head = new_player
+
+
--- test/player_test.py
+++ test/player_test.py
@@ -1,5 +1,7 @@
 import unittest
 from player import Player
+from player_list import PlayerList
+
 
 class PlayerTest(unittest.TestCase):
     def test_for_uid(self):
@@ -9,6 +11,20 @@ class PlayerTest(unittest.TestCase):
     def test_for_name(self):
         player = Player('12', 'John')
         self.assertEqual('John', player.name)
+
+    def test_for_adding_new_player_when_list_empty(self):
+        dl_list = PlayerList()
+        dl_list.push('34', 'Tom')
+        self.assertEqual(dl_list._head.key, '34')
+        self.assertIsNone(dl_list._head.next)
+
+    def test_for_adding_new_player_when_list_contains(self):
+        dl_list = PlayerList()
+        dl_list.push('14', 'Ron')
+        dl_list.push('56', 'Ann')
+        self.assertIsNotNone(dl_list._head)
+        self.assertIsNotNone(dl_list.is_empty())
+        self.assertEqual(dl_list._head.key, '56')
 
 
 if __name__ == '__main__':
